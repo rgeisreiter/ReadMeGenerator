ce7b1c90612f0e5c1a421a1fcaad400a020f52f0
rgeisreiter <rgeisreiter@yahoo.com>
2021-05-12 15:39:57 -0600

    created questions and linked page

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+var inquirer = require("inquirer");
+var fs = require("fs");
+var path = require("path");
+var generateMd = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please describe your project",
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "Which license do you want to use?",
+    name: "license",
+    choices: ["MIT", "Apache", "GPLv2", "None"],
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    writeToFile("README.md", generateMd({ ...answers }));
+  });
+}
 
 // Function call to initialize app
 init();
